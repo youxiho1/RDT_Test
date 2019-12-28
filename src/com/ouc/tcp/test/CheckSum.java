ca2bf92eca0f36077c20204364cb20eb58594c2d
youxiho1 <youxihouzainali@163.com>
2019-12-28 17:02:34 +0800

    RDT1.0
    修改eFlag以检验RDT1.0的正确性

---
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -0,0 +1,22 @@
+package com.ouc.tcp.test;
+
+import java.util.zip.CRC32;
+
+import com.ouc.tcp.message.TCP_HEADER;
+import com.ouc.tcp.message.TCP_PACKET;
+
+public class CheckSum {
+	
+	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+	public static short computeChkSum(TCP_PACKET tcpPack) {
+		int checkSum = 0;
+		
+		//计算校验和
+		
+			
+		//System.out.println(checkSum);
+		
+		return (short) checkSum;
+	}
+	
+}
